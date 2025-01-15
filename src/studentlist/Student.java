6c3697f0dd3db9129e2aff074000e3f5d4941d24
grvchh <gourav11@sheridancollege.ca>
2025-01-15 09:59:41 -0500

    initial Commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fagun at 12/05/2024
+ */
+public class Student {
+    private String name;
+    public String studentID;
+    private String address;
+    
+    public Student(String name, String studentID, String address) {
+        this.name = name;
+        this.studentID = studentID;
+        this.address = address;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+        
+     public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fagun
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student[] studentList = new Student[3];//change size before running
+        Student s1 = new Student();
+        s1.setName("Fagun Vankawala");
+        s1.studentID=1;
+        Student s2 = new Student();
+        s2.setName("Palak Shah");
+        s1.studentID=2;
+        Student s3 = new Student();
+        s3.setName("Megh Vankawala");
+        s1.studentID=3;
+//        Student s4 = new Student();
+//        s4.setName("Megh Vankawala");
+//       
+        studentList[0] = s1;
+        studentList[1] = s2;
+        studentList[2] = s3;
+        
+        //fill in 4 student names before running
+        for (Student s : studentList) {
+            System.out.println(s.studentID);
+            System.out.println(s.getName());
+        }
+
+    }
+
+}
